027dcf98a1a98fc39f9797f8efc1abfda0403f08
cleancodelover <verkyavpeter@gmail.com>
2021-10-09 03:59:52 +0100

    Create Home screen

--- screens/HomeScreen/index.tsx
+++ screens/HomeScreen/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import {View, Text} from 'react-native';
-
+import {View, Text, Image} from 'react-native';
+import styles from './styles';
+const image = require('../../assets/images/Saly-1.png');
 const HomeScreen = (props) => {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Home Screen component</Text>
+        <View style={styles.root}>
+            <Image style={styles.image} source={image} />
+            <Text style={styles.header1}>Welcome to PrimeCrypto </Text>
+            <Text style={styles.header2}>Invest your virtual $1000.00 and compete</Text>
         </View>
     )
 }
